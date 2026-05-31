fc809116180c2cf55235de4f65af1d804aaf914c
stilesloveland-cyber <stilesloveland-cyber@users.noreply.github.com>
2026-05-31 08:56:19 +0000

    fix: remove manual loadLibs() call (unnecessary for new sqlcipher-android)
    sqlcipher-android 4.6.1 automatically loads the native .so library,
    no need to call SQLiteDatabase.loadLibs() anymore.
    
    This fixes the 'Unresolved reference: loadLibs' compile error.

--- app/src/main/java/com/xclub/app/XclubApp.kt
+++ app/src/main/java/com/xclub/app/XclubApp.kt
@@ -2,12 +2,6 @@ package com.xclub.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import net.zetetic.database.sqlcipher.SQLiteDatabase
 
 @HiltAndroidApp
-class XclubApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        SQLiteDatabase.loadLibs(this)
-    }
-}
+class XclubApp : Application()
